2472e3ab4926d906cd0441ee996e1c14963bad0e
mMagicBean <whepa1038@gmail.com>
2023-05-04 14:09:30 -0400

    adding files

--- lib/spaced_rep.dart
+++ lib/spaced_rep.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:convert';
 import 'globals.dart' as globals;
 import 'package:path/path.dart' as p;
+import 'package:sqlite3/sqlite3.dart';
 
 class Review {
   late String word;
@@ -8,28 +10,101 @@ class Review {
   late File reviewFile;
 
   String revDir = 'Reviews/';
-  String revFilePath = globals.currBookName;
+
+  String tableName = globals.currBookName.replaceAll('.pdf', '').replaceAll('-', '_');
+  
   late String newPath;
+  late var db;
+
+  List<String> prevTableNames = [];
   
   Review() {
-    revFilePath = revFilePath.replaceAll('.pdf', '.txt');
-
-    newPath = p.join('$revDir', '$revFilePath');
-
-    print('newPath = $newPath');
-    
-    reviewFile = File(newPath);
+    reviewFile = File('Reviews/reviews.db');
     
     if (!reviewFile.existsSync()) {
       reviewFile.createSync(recursive: true);
     }
+
+    /*
+    db = sqlite3.open('Reviews/reviews.db');
+
+    prevTableNames.add(tableName);
+
+    for (int i=0; i < prevTableNames.length; i++) {
+      // check if table name exists already
+      if (tableName != prevTableNames[i]) {
+        db.execute('''
+          CREATE TABLE '$tableName' (
+          id INTEGER NOT NULL PRIMARY KEY,
+          word TEXT NOT NULL,
+          translation TEXT NOT NULL,
+          eval TEXT NOT NULL
+        );
+          ''');
+      }
+    }
+      */
+      
+    grabReviewsFromFile();    
   }
 
   void saveReviewToFile(String word, String translation, String review) {
-    var sink = reviewFile.openWrite(mode: FileMode.append);
+    db = sqlite3.open('Reviews/reviews.db');
     
-    sink.write('${word}' + ',' + '${translation}' + '.' + '${review}' + ';');
+    db.execute('''
+      CREATE TABLE '$tableName' (
+      id INTEGER NOT NULL PRIMARY KEY,
+      word TEXT NOT NULL,
+      translation TEXT NOT NULL,
+      eval TEXT NOT NULL
+    );
+      ''');
 
-    sink.close();
-  } 
+    
+    final wordStmt = db.prepare('INSERT INTO $tableName (word) VALUES (?)');
+    //wordStmt.execute();
+    
+    wordStmt
+      ..execute(['$word']);
+
+    wordStmt.dispose();
+      
+    final transStmt = db.prepare('INSERT INTO $tableName (translation) VALUES (?)');
+
+    transStmt
+      ..execute(['$translation']);
+
+    final evalStmt = db.prepare('INSERT INTO $tableName (eval) VALUES (?)');
+
+    evalStmt
+      ..execute(['${review}']);
+
+    final ResultSet resultSet = db.select('SELECT * FROM ${tableName}');
+
+    for (final Row row in resultSet) {
+      print('${tableName}[id: ${row['id']}, word: ${row['word']}]');
+    }
+
+    db.createFunction(
+      functionName: 'dart_version',
+      argumentCount: const AllowedArgumentCount(0),
+      function: (args) => Platform.version,
+    );
+
+    print(db.select('Select dart_version()'));
+
+    db.dispose();
+  }
+
+  void grabReviewsFromFile() {
+    var contents = reviewFile.readAsStringSync(encoding: latin1);
+
+    late List<String> word;
+    late List<String> trans;
+    late List<String> eval;
+  }
+  
+  void evaluateReviews() {
+    
+  }
 }
