f5ba5781dd655ba682d685ccc9b916612f633704
mMagicBean <whepa1038@gmail.com>
2023-05-05 06:24:53 -0400

    got inserting reviews with sqlite3 working

--- lib/spaced_rep.dart
+++ lib/spaced_rep.dart
@@ -24,14 +24,14 @@ class Review {
     if (!reviewFile.existsSync()) {
       reviewFile.createSync(recursive: true);
     }
-
-    /*
+    
     db = sqlite3.open('Reviews/reviews.db');
 
     prevTableNames.add(tableName);
 
+
+    // this is temporary
     for (int i=0; i < prevTableNames.length; i++) {
-      // check if table name exists already
       if (tableName != prevTableNames[i]) {
         db.execute('''
           CREATE TABLE '$tableName' (
@@ -43,43 +43,18 @@ class Review {
           ''');
       }
     }
-      */
+    
       
     grabReviewsFromFile();    
   }
 
   void saveReviewToFile(String word, String translation, String review) {
-    db = sqlite3.open('Reviews/reviews.db');
-    
-    db.execute('''
-      CREATE TABLE '$tableName' (
-      id INTEGER NOT NULL PRIMARY KEY,
-      word TEXT NOT NULL,
-      translation TEXT NOT NULL,
-      eval TEXT NOT NULL
-    );
-      ''');
+    final stmt = db.prepare('INSERT INTO $tableName (word, translation, eval) VALUES (?, ?, ?)');
 
-    
-    final wordStmt = db.prepare('INSERT INTO $tableName (word) VALUES (?)');
-    //wordStmt.execute();
-    
-    wordStmt
-      ..execute(['$word']);
-
-    wordStmt.dispose();
+    stmt
+      ..execute(['$word', '$translation', '$review']);
       
-    final transStmt = db.prepare('INSERT INTO $tableName (translation) VALUES (?)');
-
-    transStmt
-      ..execute(['$translation']);
-
-    final evalStmt = db.prepare('INSERT INTO $tableName (eval) VALUES (?)');
-
-    evalStmt
-      ..execute(['${review}']);
-
-    final ResultSet resultSet = db.select('SELECT * FROM ${tableName}');
+    final ResultSet resultSet = db.select('SELECT * FROM $tableName');
 
     for (final Row row in resultSet) {
       print('${tableName}[id: ${row['id']}, word: ${row['word']}]');
@@ -92,8 +67,6 @@ class Review {
     );
 
     print(db.select('Select dart_version()'));
-
-    db.dispose();
   }
 
   void grabReviewsFromFile() {
@@ -102,9 +75,5 @@ class Review {
     late List<String> word;
     late List<String> trans;
     late List<String> eval;
-  }
-  
-  void evaluateReviews() {
-    
-  }
+  }  
 }
